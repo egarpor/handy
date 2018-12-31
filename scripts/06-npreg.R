80ecf9adec4f268d0815ebae02614cd1b9914bd6
Eduardo García-Portugués <edugarpor@gmail.com>
2018-12-31 14:18:06 +0100

    v 4.3

---
+++ scripts/06-npreg.R
@@ -0,0 +1,10 @@
+
+## ------------------------------------------------------------------------
+## Name: 06-npreg.R
+## Description: Script for Chapter 6 of "Notes for Predictive Modeling"
+## Link: https://bookdown.org/egarpor/PM-UC3M/
+## License: https://creativecommons.org/licenses/by-nc-nd/4.0/
+## Author: Eduardo García-Portugués
+## ------------------------------------------------------------------------
+
+
