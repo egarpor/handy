e95f1e3c8b8f67cf0d8c63474fb075b24c6dc9f2
Eduardo García-Portugués <edugarpor@gmail.com>
2019-02-05 10:43:26 +0100

    Scripts

--- scripts/NP-EAFIT/01-intro.R
+++ scripts/NP-EAFIT/01-intro.R
@@ -1,10 +1,11 @@
 
 ## ------------------------------------------------------------------------
 ## Name: 01-intro.R
-## Description: Script for Chapter 1 of "A Short Course on Nonparametric Curve Estimation"
-## Link: https://bookdown.org/egarpor/NP-EAFIT
+## Description: Script for Chapter 1 of "Notes for Predictive Modeling"
+## Link: https://bookdown.org/egarpor/NP-UC3M/
 ## License: https://creativecommons.org/licenses/by-nc-nd/4.0/
 ## Author: Eduardo García-Portugués
 ## ------------------------------------------------------------------------
 
+## Show, by directly applying the previous definitions, that:
 
