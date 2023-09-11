cc68997a5a709d3c0ea868794dcab61aab0e5349
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2023-09-11 04:33:10 +0200

    2.0.0

--- scripts/SI-UC3M/01-preliminaries.R
+++ scripts/SI-UC3M/01-preliminaries.R
@@ -5,20 +5,21 @@
 ## Link: https://bookdown.org/egarpor/inference/
 ## License: https://creativecommons.org/licenses/by-nc-nd/4.0/
 ## Author: Isabel Molina Peralta and Eduardo García-Portugués
-## Version: 1.4.5
+## Version: 2.0.0
 ## ----------------------------------------------------------------------------
 
-## ---- toss-coin-freq-1, echo = FALSE-----------
+## ---- toss-coin-freq-1, echo = FALSE---------------------------------------------------------------
 set.seed(123456)
 n <- c(10, 20, 30, 100, 1000)
 toss <- sample(c("heads", "tails"), size = max(n), replace = TRUE)
 p <- cumsum(toss == "heads") / (1:max(n))
 knitr::kable(x = cbind(n, p[n], 1 - p[n]),
              col.names = c("$n$", "Heads", "Tails"), booktabs = TRUE,
-             caption = "Relative frequencies of \"heads\" and \"tails\" for $n$ random experiments.",
+             caption = paste("Relative frequencies of \"heads\" and",
+                             "\"tails\" for $n$ random experiments."),
              digits = 3, align = c("lcc"), escape = FALSE)
 
-## ---- car-accidents-1, echo = FALSE, cache = TRUE----
+## ---- car-accidents-1, echo = FALSE, cache = TRUE--------------------------------------------------
 set.seed(123456)
 lambda <- 4
 n <- c(10, 20, 30, 100, 1000, 10000)
@@ -27,21 +28,23 @@ rel_freq_accidents <- sapply(0:6,
                             function(k) cumsum(k == accidents) / (1:max(n)))
 knitr::kable(x = cbind(n, rel_freq_accidents[n, ]),
              col.names = c("$n$", paste0("$", 0:5, "$"), "$\\geq 6$"),
-             caption = "Relative frequencies of car accidents in Spain for $n$ hours.",
+             caption = paste("Relative frequencies of car accidents",
+                             "in Spain for $n$ hours."),
              booktabs = TRUE, digits = 3, align = c("lccccccccc"),
              escape = FALSE)
 
-## ---- pedestrian-1, echo = FALSE, cache = TRUE----
+## ---- pedestrian-1, echo = FALSE, cache = TRUE-----------------------------------------------------
 set.seed(123456)
 n <- c(10, 20, 30, 100, 1000, 5000)
 weights <- rnorm(max(n), mean = 49, sd = 5)
 intervals <- cut(weights, breaks = c(0, 35, 45, 55, 65, Inf))
-rel_freq_intervals <- sapply(levels(intervals),
-                             function(w) cumsum(w == intervals) / (1:max(n)))
+rel_freq_intervals <- sapply(levels(intervals), function(w)
+  cumsum(w == intervals) / (1:max(n)))
 knitr::kable(x = cbind(n, rel_freq_intervals[n, ]),
              col.names = c("$n$", "$[0, 35)$", "$[35, 45)$", "$[45, 55)$",
                            "$[55, 65)$", "$[65, \\infty)$"),
-             caption = "Relative frequencies of weight intervals for $n$ measured pedestrians.",
+             caption = paste("Relative frequencies of weight intervals",
+                             "for $n$ measured pedestrians."),
              booktabs = TRUE, digits = 3, align = c("lccccc"),
              escape = FALSE)
 
--- scripts/SI-UC3M/05-confidence-intervals.R
+++ scripts/SI-UC3M/05-confidence-intervals.R
@@ -5,28 +5,28 @@
 ## Link: https://bookdown.org/egarpor/inference/
 ## License: https://creativecommons.org/licenses/by-nc-nd/4.0/
 ## Author: Isabel Molina Peralta and Eduardo García-Portugués
-## Version: 1.4.5
+## Version: 2.0.0
 ## ----------------------------------------------------------------------------
 
-## ---- ci-mu-1----------------------------------
+## ---- ci-mu-1--------------------------------------------------------------------------------------
 # Sample
-X <- c(3005, 2925, 2935, 2965, 2995, 3005, 2937, 2905)
+X <- c(916, 892, 895, 904, 913, 916, 895, 885)
 
 # n, mean, sigma, alpha, z_{\alpha/2}
 n <- length(X)
 X_bar <- mean(X)
-sigma <- 39
+sigma <- 12
 alpha <- 0.05
 z <- qnorm(alpha / 2, lower.tail = FALSE)
 
 # CI
 X_bar + c(-1, 1) * z * sigma / sqrt(n)
 
-## ---- ci-mu-2----------------------------------
+## ---- ci-mu-2--------------------------------------------------------------------------------------
 # Sample
 X <- c(3005, 2925, 2935, 2965, 2995, 3005, 2937, 2905)
 
-# n, mean, S', alpha, z_{\alpha/2}
+# n, mean, S', alpha, t_{\alpha/2;n-1}
 n <- length(X)
 X_bar <- mean(X)
 S_prime <- sd(X)
@@ -36,7 +36,7 @@ t <- qt(alpha / 2, df = n - 1, lower.tail = FALSE)
 # CI
 X_bar + c(-1, 1) * t * S_prime / sqrt(n)
 
-## ---- ci-sigma2--------------------------------
+## ---- ci-sigma2------------------------------------------------------------------------------------
 # Sample
 X <- c(4.1, 5.2, 10.2)
 
@@ -50,7 +50,7 @@ c2 <- qchisq(alpha / 2, df = n - 1, lower.tail = FALSE)
 # CI
 (n - 1) * S2_prime / c(c2, c1)
 
-## ---- ci-diff-mu-1-----------------------------
+## ---- ci-diff-mu-1---------------------------------------------------------------------------------
 # Samples
 X_1 <- c(32, 37, 35, 28, 41, 44, 35, 31, 34)
 X_2 <- c(35, 31, 29, 25, 34, 40, 27, 32, 31)
@@ -67,12 +67,12 @@ z <- qnorm(alpha / 2, lower.tail = FALSE)
 # CI
 (X_bar_1 - X_bar_2) + c(-1, 1) * z * sqrt(sigma2_1 / n_1 + sigma2_2 / n_2)
 
-## ---- ci-diff-mu-2-----------------------------
+## ---- ci-diff-mu-2---------------------------------------------------------------------------------
 # Samples
 X_1 <- c(32, 37, 35, 28, 41, 44, 35, 31, 34)
 X_2 <- c(35, 31, 29, 25, 34, 40, 27, 32, 31)
 
-# n1, n2, Xbar1, Xbar2, S^2, alpha, z_{alpha/2}
+# n1, n2, Xbar1, Xbar2, S^2, alpha, t_{alpha/2;n1-n2-2}
 n_1 <- length(X_1)
 n_2 <- length(X_2)
 X_bar_1 <- mean(X_1)
@@ -86,7 +86,7 @@ t <- qt(alpha / 2, df = n_1 + n_2 - 2, lower.tail = FALSE)
 # CI
 (X_bar_1 - X_bar_2) + c(-1, 1) * t * S * sqrt(1 / n_1 + 1 / n_2)
 
-## ---- ratio-vars-------------------------------
+## ---- ratio-vars-1---------------------------------------------------------------------------------
 # n1, n2, S1'^2, S2'^2, alpha, c1, c2
 n_1 <- 11
 n_2 <- 14
@@ -99,7 +99,17 @@ c2 <- qf(alpha / 2, df1 = n_1 - 1, df2 = n_2 - 1, lower.tail = FALSE)
 # CI
 (S2_prime_1 / S2_prime_2) / c(c2, c1)
 
-## ---- aci-1------------------------------------
+## ---- ratio-vars-2---------------------------------------------------------------------------------
+# Xbar1, Xbar2, S^2, t_{alpha/2;n1-n2-2}
+X_bar_1 <- 64
+X_bar_2 <- 69
+S2 <- ((n_1 - 1) * S2_prime_1 + (n_2 - 1) * S2_prime_2) / (n_1 + n_2 - 2)
+t <- qt(1 - alpha / 2, df = n_1 + n_2 - 2, lower.tail = FALSE)
+
+# CI
+(X_bar_1 - X_bar_2) + c(-1, 1) * t * sqrt(S2 * (1 / n_1 + 1 / n_2))
+
+## ---- aci-1----------------------------------------------------------------------------------------
 # Sample from Exp(2)
 set.seed(123456)
 n <- 100
@@ -113,9 +123,9 @@ alpha <- 0.05
 z <- qnorm(alpha / 2, lower.tail = FALSE)
 lambda_mle + c(-1, 1) * z * lambda_mle / sqrt(n)
 
-## ---- ci-1-------------------------------------
-# Percentile bootstrap confidence intervals. It requires a sample x and an
-# estimator theta_hat() (must be a function!).
+## ---- ci-1-----------------------------------------------------------------------------------------
+# Computation of percentile bootstrap confidence intervals. It requires a
+# sample x and an estimator theta_hat() (must be a function!).
 boot_ci <- function(x, theta_hat, B = 5e3, alpha = 0.05, plot_boot = TRUE) {
 
   # Check that theta_hat is a proper function
@@ -124,16 +134,17 @@ boot_ci <- function(x, theta_hat, B = 5e3, alpha = 0.05, plot_boot = TRUE) {
   # Creates convenience statistic for boot::boot()
   stat <- function(x, indexes) theta_hat(x[indexes])
 
-  # Perform bootstrap resampling with the aid of boot::boot()
+  # Perform bootstrap resampling (step 2) with the aid of boot::boot()
   boot_obj <- boot::boot(data = x, statistic = stat, sim = "ordinary", R = B)
 
-  # Extract bootstrapped statistics from the boot object
+  # Extract bootstrapped statistics (theta_hat_star's) from the boot object
   theta_hat_star <- boot_obj$t
 
   # Confidence intervals
   ci <- quantile(theta_hat_star, probs = c(alpha / 2, 1 - alpha / 2))
 
   # Plot the distribution of bootstrap estimators and the confidence intervals?
+  # Draw also theta_hat, the statistic from the original sample, in boot_obj$t0
   if (plot_boot) {
 
     hist(theta_hat_star, probability = TRUE, main = "",
@@ -149,21 +160,28 @@ boot_ci <- function(x, theta_hat, B = 5e3, alpha = 0.05, plot_boot = TRUE) {
 
 }
 
-## ---- ci-2-------------------------------------
+## ---- ci-2-----------------------------------------------------------------------------------------
 # Sample from Exp(2)
 set.seed(123456)
 n <- 200
 x <- rexp(n = n, rate = 2)
 
-## ---- ci-3-------------------------------------
+## ---- ci-3-----------------------------------------------------------------------------------------
 # Bootstrap confidence interval
 lambda_hat_exp <- function(x) 1 / mean(x)
 boot_ci(x = x, B = 5e3, theta_hat = lambda_hat_exp)
 
-## ---- ci-4-------------------------------------
+## ---- ci-4-----------------------------------------------------------------------------------------
 # Asymptotic confidence interval
 alpha <- 0.05
 z <- qnorm(alpha / 2, lower.tail = FALSE)
 lambda_mle <- 1 / mean(x)
 lambda_mle + c(-1, 1) * z * lambda_mle / sqrt(n)
+
+## ---- ci-5-----------------------------------------------------------------------------------------
+# Sample
+X <- c(916, 892, 895, 904, 913, 916, 895, 885)
+
+# Bootstrap confidence interval
+boot_ci(x = X, B = 5e3, theta_hat = mean)
 
