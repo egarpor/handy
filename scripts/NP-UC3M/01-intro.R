33b8a2733a0c7af0374ef27588ef5687758bb382
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2025-12-20 12:55:34 +0100

    Update scripts

--- scripts/NP-UC3M/01-intro.R
+++ scripts/NP-UC3M/01-intro.R
@@ -5,7 +5,7 @@
 ## Link: https://bookdown.org/egarpor/NP-UC3M/
 ## License: https://creativecommons.org/licenses/by-nc-nd/4.0/
 ## Author: Eduardo García-Portugués
-## Version: 6.12.5
+## Version: 6.12.9
 ## ----------------------------------------------------------------------------
 
 ## ----ohsfig, echo = FALSE, fig.cap = '(ref:ohsfig-title)', fig.margin = FALSE, purl = TRUE, fig.asp = 3/2, fig.pos = 'h!'----
--- scripts/NP-UC3M/05-kre-ii.R
+++ scripts/NP-UC3M/05-kre-ii.R
@@ -5,10 +5,10 @@
 ## Link: https://bookdown.org/egarpor/NP-UC3M/
 ## License: https://creativecommons.org/licenses/by-nc-nd/4.0/
 ## Author: Eduardo García-Portugués
-## Version: 6.12.5
+## Version: 6.12.9
 ## ----------------------------------------------------------------------------
 
-## ----mult-1, eval = knitr:::is_html_output(), cache = TRUE----
+## ----mult-1, eval = knitr:::is_html_output(), cache = TRUE-------------------
 # Sample data from a bivariate regression
 n <- 300
 set.seed(123456)
@@ -46,7 +46,7 @@ rgl::surface3d(x = x_grid, y = y_grid,
                col = "green", alpha = 0.25, lit = FALSE)
 rgl::rglwidget()
 
-## ----mult-2----------------------------------------------
+## ----mult-2------------------------------------------------------------------
 # Load the wine dataset
 wine <- read.table(file = "datasets/wine.csv", header = TRUE, sep = ",")
 
@@ -81,7 +81,7 @@ apply(wine[c("Age", "WinterRain", "AGST", "HarvestRain")], 2, median)
 #   quadratic pattern
 # - HarvestRain is negatively related to Price (with a linear-like relation)
 
-## ----mult-3----------------------------------------------
+## ----mult-3------------------------------------------------------------------
 # The argument "xq" controls the conditioning quantile of the predictors, by
 # default the median (xq = 0.5). But xq can be a vector of p quantiles, for
 # example (0.25, 0.5, 0.25, 0.75) for (Age, WinterRain, AGST, HarvestRain)
@@ -115,8 +115,8 @@ legend("topleft", legend = latex2exp::TeX(paste0("$\\tau =", tau, "$")),
 # These quantiles are
 apply(wine[c("Age", "WinterRain", "HarvestRain")], 2, quantile, prob = tau)
 
-## ----mix-1, fig.margin = FALSE, fig.asp = 1/2------------
-# Bandwidth by CV for local linear estimator
+## ----mix-1, fig.margin = FALSE, fig.asp = 1/2--------------------------------
+# Bandwidth by CV for local constant estimator
 # Recall that Species is a factor!
 bw_iris <- np::npregbw(formula = Petal.Length ~ Sepal.Width + Species,
                        data = iris)
@@ -136,7 +136,7 @@ plot(fit_iris, plot.par.mfrow = FALSE)
 par(mfrow = c(1, 2))
 plot(fit_iris, xq = 0.9, plot.par.mfrow = FALSE)
 
-## ----mix-2, fig.fullwidth = TRUE, fig.margin = FALSE, fig.asp = 2/3----
+## ----mix-2, fig.fullwidth = TRUE, fig.margin = FALSE, fig.asp = 2/3----------
 # Load data
 data(oecdpanel, package = "np")
 
@@ -172,7 +172,7 @@ summary(fit_OECD)
 par(mfrow = c(2, 3))
 plot(fit_OECD, plot.par.mfrow = FALSE)
 
-## ----predci-1, fig.fullwidth = TRUE, fig.margin = FALSE, fig.asp = 2/3----
+## ----predci-1, fig.fullwidth = TRUE, fig.margin = FALSE, fig.asp = 2/3-------
 # Asymptotic confidence bands for the marginal effects of each predictor on
 # the response
 par(mfrow = c(2, 3))
@@ -206,7 +206,7 @@ cbind(pred$fit - z_alpha2 * pred$se.fit, pred$fit + z_alpha2 * pred$se.fit)
 # Recall that z_alpha2 is almost 2
 z_alpha2
 
-## ----predci-2, fig.fullwidth = TRUE, fig.margin = FALSE, fig.asp = 2/3----
+## ----predci-2, fig.fullwidth = TRUE, fig.margin = FALSE, fig.asp = 2/3-------
 # Bootstrap confidence bands (using naive bootstrap, the default)
 # They take more time to compute because a resampling + refitting takes place
 B <- 200
@@ -218,7 +218,7 @@ plot(fit_OECD, plot.errors.method = "bootstrap", common.scale = FALSE,
 # random.seed fixes the seed to always get the same bootstrap errors. It
 # defaults to 42 if not specified
 
-## ----predci-3--------------------------------------------
+## ----predci-3----------------------------------------------------------------
 # Univariate local constant regression with CV bandwidth
 bw1 <- np::npregbw(formula = growth ~ initgdp, data = oecdpanel, regtype = "lc")
 fit1 <- np::npreg(bw1)
@@ -255,15 +255,13 @@ lines(npplot_qua$r1$eval[, 1], npplot_qua$r1$mean + npplot_qua$r1$merr[, 2],
 # interval method)
 head(npplot_qua$r1$merr)
 
-# There is no predict() method featuring bootstrap confidence intervals,
-# it has to be coded manually!
-
-# Function to predict and compute confidence intervals for m(x). Takes as main
-# arguments a np::npreg object (npfit) and the values of the predictors where
-# to carry out prediction (exdat). Requires that exdat is a data.frame of the
-# same type than the one used for the predictors (e.g., there will be an error
-# if one variable appears as a factor for computing npfit and then is passed
-# as a numeric in exdat)
+## ----predci-4----------------------------------------------------------------
+# Function to predict and compute bootstrap confidence intervals for m(x). Takes
+# as main arguments a np::npreg object (npfit) and the values of the predictors
+# where to carry out prediction (exdat). Requires that exdat is a data.frame of
+# the same type than the one used for the predictors (e.g., there will be an
+# error if one variable appears as a factor for computing npfit and then is
+# passed as a numeric in exdat)
 np_pred_CI <- function(npfit, exdat, B = 200, conf = 0.95,
                        type_CI = c("standard", "quantiles")[1]) {
 
@@ -373,7 +371,7 @@ filled.contour(x_initgdp, x_popgro, ci_dif, nlevels = 20,
                  points(popgro ~ initgdp, data = oecdpanel, pch = 16)
                  })
 
-## ----ll-1, eval = TRUE-----------------------------------
+## ----ll-1, eval = TRUE-------------------------------------------------------
 # Simulate some data
 n <- 200
 logistic <- function(x) 1 / (1 + exp(-x))
@@ -423,7 +421,7 @@ lines(x, predict(fit_locfit, newdata = x), col = 4)
 legend("topright", legend = c("p(x)", "glm", "nlm", "locfit"),
        lwd = 2, col = c(1, 2, 3, 4), lty = c(1, 1, 2, 1))
 
-## ----ll-2, eval = TRUE-----------------------------------
+## ----ll-2, eval = TRUE-------------------------------------------------------
 # Exact LCV - recall that we *maximize* the LCV!
 h <- seq(0.1, 2, by = 0.1)
 suppressWarnings(
